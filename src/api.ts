ce20835498218b93bba6fe5124ae6512181c863c
cheechyuanang <angchee@live.com.my>
2024-03-16 23:19:35 +0000

    change port number

--- src/api.ts
+++ src/api.ts
@@ -84,8 +84,8 @@ const main = () => {
       .json({ smartAccountAddress: smartAccountAddress });
   });
 
-  app.listen(3000, () => {
-    console.log("Server started on port 3000");
+  app.listen(3001, () => {
+    console.log("Server started on port 3001");
   });
 };
 
